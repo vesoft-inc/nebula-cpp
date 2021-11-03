09ccd8f404a574a4a922635c07b35f9f4918365e
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2021-11-03 17:22:07 +0800

    Remove common clause.

--- .linters/cpp/hooks/pre-commit.sh
+++ .linters/cpp/hooks/pre-commit.sh
@@ -3,7 +3,6 @@
 # Copyright (c) 2018 vesoft inc. All rights reserved.
 #
 # This source code is licensed under Apache 2.0 License,
-# attached with Common Clause Condition 1.0, found in the LICENSES directory.
 
 CPPLINT=`dirname $0`/../../.linters/cpp/cpplint.py
 
