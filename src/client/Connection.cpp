e655b95973f5c6eb69ab3a92ccd0d919dd32284f
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2021-12-07 11:11:48 +0800

    Tune test.

--- src/client/Connection.cpp
+++ src/client/Connection.cpp
@@ -101,7 +101,7 @@ bool Connection::open(const std::string &address,
         }
       });
   // Connection is not stable in some environments, so we need to wait a while
-  std::this_thread::sleep_for(std::chrono::milliseconds(300));
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
   return complete;
 }
 
