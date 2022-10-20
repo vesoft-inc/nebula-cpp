dad0939022d565354b753737918e26d5a0b889c7
shylock <33566796+Shylock-Hg@users.noreply.github.com>
2022-10-20 11:49:28 +0800

    Add session pool example. (#107)
    * Add session pool example.
    
    * Fix typo.
    
    * Fix compile.
    
    * Expose session pool symbols.

---
+++ examples/SessionPoolExample.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2022 vesoft inc. All rights reserved.
+//
+// This source code is licensed under Apache 2.0 License.
+
+#include <common/Init.h>
+#include <nebula/client/SessionPool.h>
+
+#include <atomic>
+#include <chrono>
+#include <thread>
+#include "nebula/client/Config.h"
+
+int main(int argc, char* argv[]) {
+  nebula::init(&argc, &argv);
+  {
+    // Create space
+    nebula::ConnectionPool pool;
+    auto config = nebula::Config{};
+    config.maxConnectionPoolSize_ = 1;
+    pool.init({"127.0.0.1:9669"}, config);
+    auto session = pool.getSession("root", "nebula");
+    assert(session.valid());
+
+    auto resp = session.execute(
+        "CREATE SPACE IF NOT EXISTS session_pool_test(vid_type = FIXED_STRING(16));");
+    assert(resp.errorCode == nebula::ErrorCode::SUCCEEDED);
+  }
+
+  nebula::SessionPoolConfig config;
+  config.username_ = "root";
+  config.password_ = "nebula";
+  config.addrs_ = {"127.0.0.1:9669"};
+  config.spaceName_ = "session_pool_test";
+  config.maxSize_ = 10;
+  nebula::SessionPool pool(config);
+  pool.init();
+
+  std::vector<std::thread> threads;
+  for (std::size_t i = 0; i < config.maxSize_; ++i) {
+    threads.emplace_back([&pool]() {
+      auto resp = pool.execute("YIELD 1");
+      assert(resp.errorCode == nebula::ErrorCode::SUCCEEDED);
+      std::cout << "Result: " << *resp.data << std::endl;
+    });
+  }
+  for (auto& t : threads) {
+    t.join();
+  }
+}
